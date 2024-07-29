f65c8a11376aebe0fce5b63b5408870249aefd73
user <user@MacBook-Pro.local>
2024-07-29 23:12:01 +0100

    js data type and  common operations

---
+++ app.js
@@ -0,0 +1,60 @@
+console.log("hello world");
+
+// Data type
+/**
+ * Number, number = 5
+ * String, name = 'Samuel'
+ * boolean = 'Married?' = true
+ * null = 
+ * undefine = 
+ * Arrays = [] = number of children
+ * Objects = person
+ * float = interest rate (5.45)
+ */
+
+// Operations
+/**
+ * Addition (+)
+ * subtraction (-)
+ * Division (/)
+ * Multiplication (*)
+ * equality (==)
+ * Strict equality (===)
+ * Concatenation for strings (+)
+ */
+
+
+const age = 60;
+const name = 'Emmanuel';
+const married = true;
+const listOfBusinesses = ['Transport business', 'Food business'];
+
+const car = {
+    brandName: 'Camry',
+    meter: 1000.3,
+    yearOfProduction: 2022,
+    fourWheels: true,
+    interiror:['stiring', 'seat']
+}
+
+
+const person = {
+    name: 'samuel',
+    married: true,
+    listOfBusinesses: ['Transport business', 'Food business'],
+    bankBalance: 500.50,
+    yearOfBirth: 1994,
+    age: 60,
+    numberOfYearsToRetire: this.yearOfBirth - this.age
+}
+
+
+const ListOfEnterprise = ['Transport business', 'Food business'];
+
+
+
+
+
+
+
+
